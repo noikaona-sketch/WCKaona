c7b44b9f771f44af26d4b78bcd9bda7a3614fb01
noikaona-sketch <noikaona@gmail.com>
2026-06-06 18:18:45 +0700

    Fix storage migration foldername syntax

--- supabase/migrations/20260606002000_storage_foundation.sql
+++ supabase/migrations/20260606002000_storage_foundation.sql
@@ -33,9 +33,9 @@ create policy wood_receipts_storage_upload_authenticated
   to authenticated
   with check (
     bucket_id = 'wood-receipts'
-    and storage.foldername(name)[1] = 'receipt'
-    and storage.foldername(name)[2] is not null
-    and storage.foldername(name)[2] <> ''
+    and (storage.foldername(name))[1] = 'receipt'
+    and (storage.foldername(name))[2] is not null
+    and (storage.foldername(name))[2] <> ''
     and array_length(storage.foldername(name), 1) = 2
     and storage.filename(name) in ('01_size.jpg', '02_moisture.jpg', '03_license.jpg')
   );
